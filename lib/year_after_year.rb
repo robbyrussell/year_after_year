7607c13e8e5289e5a1937fbf2cc2686f791f34f2
Robby Russell <robby@planetargon.com>
2008-03-24 21:35:16 -0700

    Adding a year_range method upon request

--- lib/year_after_year.rb
+++ lib/year_after_year.rb
@@ -5,5 +5,13 @@ module PlanetArgon
     def current_year
       Time.now.strftime('%Y')
     end
+    
+    def year_range(start_year)
+      if start_year.to_s == current_year
+        current_year
+      else
+        start_year.to_s + '-' + current_year
+      end
+    end
   end
 end
--- spec/year_after_year_spec.rb
+++ spec/year_after_year_spec.rb
@@ -10,3 +10,18 @@ describe PlanetArgon::YearAfterYear, 'current_year' do
     current_year.should == '2006'
   end
 end
+
+describe PlanetArgon::YearAfterYear, 'year_range()' do
+  before(:each) do
+    t = mock('Time')
+    Time.stub!(:now).and_return( Time.parse( 'Sun Jan 27 15:31:39 -0800 2006' ) ) 
+  end
+  
+  it 'should return a range of years when provided a start year' do
+    year_range(2004).should == '2004-2006'
+  end
+  
+  it 'should return only the current year if the start year is the same' do
+    year_range(2006).should == '2006'
+  end  
+end
